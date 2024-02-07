29312977edc5e2aa2fbd62c0d321ce9658db98b4
Michael C <mc26@open.ac.uk>
2024-02-07 12:23:27 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace MopriaScannerClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtDocumentId = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ScanBufferInfo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(40, 329);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtStatus.Size = new System.Drawing.Size(634, 246);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ScannerStatus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(369, 42);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(107, 20);
+            this.txtIP.TabIndex = 3;
+            this.txtIP.Text = "192.168.1.67";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(40, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Scan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtDocumentId
+            // 
+            this.txtDocumentId.Location = new System.Drawing.Point(252, 200);
+            this.txtDocumentId.Name = "txtDocumentId";
+            this.txtDocumentId.Size = new System.Drawing.Size(410, 20);
+            this.txtDocumentId.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(40, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Get Scanned Info";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(40, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Get Scanned Image Data";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.picBox);
+            this.panel1.Location = new System.Drawing.Point(707, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 556);
+            this.panel1.TabIndex = 9;
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Bisque;
+            this.picBox.Location = new System.Drawing.Point(3, 2);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(220, 108);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBox.TabIndex = 10;
+            this.picBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Scanner IP Address";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(40, 69);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "ScannerCapabilities";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Status / Data:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1307, 636);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtDocumentId);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Mopria Scanner Test";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtDocumentId;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+
+
+namespace MopriaScannerClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            txtStatus.Text = "";
+            string url = "http://"+txtIP.Text.Trim()+"/eSCL/ScannerStatus";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make a synchronous GET request
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and return the response content
+                        string resp= response.Content.ReadAsStringAsync().Result;
+                        txtStatus.Text = resp;
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a GET request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                txtStatus.Text = $"{url} Error while making the request: {ex.Message}";
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //crashes printer
+
+            string url = "http://" + txtIP.Text.Trim() + "/eSCL/ScanBufferInfo";
+            string payload = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<scan:ScanSettings xmlns:scan=""http://schemas.hp.com/imaging/escl/2011/05/03"" xmlns:pwg=""http://www.pwg.org/schemas/2010/12/sm"">
+   <pwg:Version>2.0</pwg:Version>
+   <pwg:ScanRegions>
+      <pwg:ScanRegion>
+         <pwg:Height>3500</pwg:Height>
+         <pwg:Width>2550</pwg:Width>
+         <pwg:XOffset>0</pwg:XOffset>
+         <pwg:YOffset>0</pwg:YOffset>
+      </pwg:ScanRegion>
+   </pwg:ScanRegions>
+   <scan:XResolution>100</scan:XResolution>
+   <scan:YResolution>100</scan:YResolution>
+   <scan:ColorMode>RGB24</scan:ColorMode>
+</scan:ScanSettings>
+";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Create a StringContent with the XML payload
+                    StringContent content = new StringContent(payload, Encoding.UTF8, "text/xml");
+
+                    // Make a synchronous PUT request
+                    HttpResponseMessage response = client.PutAsync(url, content).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and return the response content
+                        string resp =response.Content.ReadAsStringAsync().Result;
+                        txtStatus.Text = resp;
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a PUT request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                txtStatus.Text = $"{url} Error while making the request: {ex.Message}";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtDocumentId.Text = "";
+            txtStatus.Text = "";
+            string url = "http://" + txtIP.Text.Trim() + "/eSCL/ScanJobs";
+            string payload = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<scan:ScanSettings xmlns:scan=""http://schemas.hp.com/imaging/escl/2011/05/03"" xmlns:pwg=""http://www.pwg.org/schemas/2010/12/sm"">
+   <pwg:Version>2.0</pwg:Version>
+   <pwg:ScanRegions>
+      <pwg:ScanRegion>
+         <pwg:Height>3500</pwg:Height>
+         <pwg:Width>2550</pwg:Width>
+         <pwg:XOffset>0</pwg:XOffset>
+         <pwg:YOffset>0</pwg:YOffset>
+      </pwg:ScanRegion>
+   </pwg:ScanRegions>
+   <scan:DocumentFormatExt>image/jpeg</scan:DocumentFormatExt>
+   <pwg:ContentType>TextAndPhoto</pwg:ContentType>
+   <scan:XResolution>100</scan:XResolution>
+   <scan:YResolution>100</scan:YResolution>
+   <scan:ColorMode>RGB24</scan:ColorMode>
+</scan:ScanSettings>
+";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Create a StringContent with the XML payload
+                    StringContent content = new StringContent(payload, Encoding.UTF8, "text/xml");
+
+                    // Make a synchronous PUT request
+                    HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and return the response content
+                        string resp = response.Content.ReadAsStringAsync().Result;
+                        txtStatus.Text = resp + "Done\n" + "Headers:" + response.Headers.ToString();
+
+                        // Get header  Location:   to get URL of scan data  
+                        Dictionary<string, string> hdrs       = response.Headers.ToDictionary(a => a.Key, a => string.Join(";", a.Value));
+                        txtDocumentId.Text = hdrs["Location"];
+
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a Post request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                txtStatus.Text = $"{url} Error while making the request: {ex.Message}";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            txtStatus.Text = "";
+            string url = txtDocumentId.Text+ "/ScanImageInfo";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make a synchronous GET request
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and return the response content
+                        string resp = response.Content.ReadAsStringAsync().Result;
+                        txtStatus.Text = resp;
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a GET request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                txtStatus.Text=$"{url} Error while making the GET request: {ex.Message}";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            txtStatus.Text = "";
+            string url = txtDocumentId.Text + "/NextDocument";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make a synchronous GET request
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        // Read the image data as a byte array
+                        byte[] imageData = response.Content.ReadAsByteArrayAsync().Result;
+
+                        // Create a MemoryStream from the byte array
+                        using (MemoryStream stream = new MemoryStream(imageData))
+                        {
+                            // Create an Image from the MemoryStream
+                            Image image = Image.FromStream(stream);
+
+                            // Set the PictureBox image
+                            picBox.Image = image;
+
+                            txtStatus.Text = $"OK:  Image size retrieved: {image.Size.Width} x {image.Size.Height}\n   Format:{image.RawFormat.ToString()}";
+                        }
+
+                        
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a GET request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                txtStatus.Text = $"{url} Error while making the GET request: {ex.Message}";
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text = "";
+            string url = "http://" + txtIP.Text.Trim() + "/eSCL/ScannerCapabilities";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make a synchronous GET request
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and return the response content
+                        string resp = response.Content.ReadAsStringAsync().Result;
+                        txtStatus.Text = resp;
+                    }
+                    else
+                    {
+                        // Handle the case when the request was not successful
+                        throw new Exception($"Failed to make a GET request. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the HTTP request
+                throw new Exception($"Error while making the GET request: {ex.Message}");
+            }
+        }
+    }
+}
